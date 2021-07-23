26ec89d76a2bed3c083c9ae5525dca302dda10ca
Zhang Ziqing <zhangziqing9926@gmail.com>
2021-07-24 03:25:22 +0800

    fix: add main to jiojiobot.py

--- jiojiobot.py
+++ jiojiobot.py
@@ -6,13 +6,14 @@ import requests
 import logging
 from functions import *
 import os
-ON_HEROKU = os.environ.get('ON_HEROKU')
-if ON_HEROKU:
-    # get the heroku port
-    # as per OP comments default is 17995
-    port = int(os.environ.get("PORT", 17995))
-else:
-    port = 6379  # 3000
+PORT = int(os.environ.get('PORT', '8443'))
+# ON_HEROKU = os.environ.get('ON_HEROKU')
+# if ON_HEROKU:
+#     # get the heroku port
+#     # as per OP comments default is 17995
+#     port = int(os.environ.get("PORT", 17995))
+# else:
+#     port = 6379  # 3000
 # PORT = int(os.environ.get('PORT', 8443))
 
 # r = redis.Redis(host='localhost', port=6379)
@@ -23,50 +24,57 @@ bot_token = "1843778002:AAFxOoXX7xWTubiMmULqkTlQ9U0267zHa4I"
 # Setting up our logger
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# Setting up their polling stuff
-updater = Updater(token=bot_token, use_context=True)
-dispatcher = updater.dispatcher
 
-# Create and add command handlers
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def main():
+    # Setting up their polling stuff
+    updater = Updater(token=bot_token, use_context=True)
+    dispatcher = updater.dispatcher
 
-start_jio_handler = CommandHandler('start_jio', start_jio)
-dispatcher.add_handler(start_jio_handler)
+    # Create and add command handlers
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
 
-edit_jio_title_handler = CommandHandler('edit_jio_title', edit_jio_title)
-dispatcher.add_handler(edit_jio_title_handler)
+    start_jio_handler = CommandHandler('start_jio', start_jio)
+    dispatcher.add_handler(start_jio_handler)
 
-join_jio_handler = CommandHandler("join_jio", join_jio)
-dispatcher.add_handler(join_jio_handler)
+    edit_jio_title_handler = CommandHandler('edit_jio_title', edit_jio_title)
+    dispatcher.add_handler(edit_jio_title_handler)
 
-finalise_jio_handler = CommandHandler('finalise_jio', finalise_jio)
-dispatcher.add_handler(finalise_jio_handler)
+    join_jio_handler = CommandHandler("join_jio", join_jio)
+    dispatcher.add_handler(join_jio_handler)
 
-paid_handler = CommandHandler("paid", paid)
-dispatcher.add_handler(paid_handler)
+    finalise_jio_handler = CommandHandler('finalise_jio', finalise_jio)
+    dispatcher.add_handler(finalise_jio_handler)
 
-end_jio_handler = CommandHandler('end_jio', end_jio)
-dispatcher.add_handler(end_jio_handler)
+    paid_handler = CommandHandler("paid", paid)
+    dispatcher.add_handler(paid_handler)
 
-quit_jio_handler = CommandHandler('quit_jio', quit_jio)
-dispatcher.add_handler(quit_jio_handler)
+    end_jio_handler = CommandHandler('end_jio', end_jio)
+    dispatcher.add_handler(end_jio_handler)
 
-confirm_finalise_handler = CallbackQueryHandler(
-    callback=confirm_finalise_jio, pattern=r'confirm_finalise')
-dispatcher.add_handler(confirm_finalise_handler)
+    quit_jio_handler = CommandHandler('quit_jio', quit_jio)
+    dispatcher.add_handler(quit_jio_handler)
 
-cancel_handler = CallbackQueryHandler(callback=cancel, pattern=r'cancel')
-dispatcher.add_handler(cancel_handler)
+    confirm_finalise_handler = CallbackQueryHandler(
+        callback=confirm_finalise_jio, pattern=r'confirm_finalise')
+    dispatcher.add_handler(confirm_finalise_handler)
 
-# updater.start_polling()
+    cancel_handler = CallbackQueryHandler(callback=cancel, pattern=r'cancel')
+    dispatcher.add_handler(cancel_handler)
 
-# # Start the Bot
-updater.start_webhook(listen="0.0.0.0",
-                      port=int(port),
-                      url_path=bot_token)
-updater.bot.setWebhook(
-    'https://floating-thicket-85827.herokuapp.com/' + bot_token)
+    # updater.start_polling()
 
-updater.idle()
+    # # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=bot_token)
+    updater.bot.setWebhook(
+        'https://floating-thicket-85827.herokuapp.com/' + bot_token)
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
