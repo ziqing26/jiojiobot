2261c514b36d769c2b823cda73adc1eb622ed36d
Zhang Ziqing <zhangziqing9926@gmail.com>
2021-07-24 10:16:01 +0800

    feat: deployed

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
-import redis
 from util import *
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ParseMode
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ParseMode 
 import os
+import redis
 
 """
 Terminologies:
@@ -27,24 +27,19 @@ item [hmset]
 
 """
 
-# r = redis.Redis(host='localhost', decode_responses=True, port=6379)
-r = redis.from_url(os.environ.get("REDIS_URL"))
 
 
 def start(update, context):
-    r = redis.from_url(os.environ.get("REDIS_URL"))
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text="Hi! To start a jio, type '/start_jio'")
 
 # Create a new jio with a title
-
-
 def start_jio(update, context):
-    r = redis.from_url(os.environ.get("REDIS_URL"))
-    arguments, user_id, user_name, chat_id = parse(update, context)
-    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(
-        chat_id, user_id)
+    r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
 
+    arguments, user_id, user_name, chat_id = parse(update, context)
+    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(chat_id, user_id)
+    
     if r.hget(chat_id_meta_string, 'message_id'):
         context.bot.send_message(chat_id=chat_id,
                                  text="There is an ongoing jio. To end the jio, use '/end_jio'")
@@ -53,39 +48,36 @@ def start_jio(update, context):
         context.bot.send_message(chat_id=chat_id,
                                  text="To start a jio, type '/start_jio <title>'")
         return
-
+    
     # Send jio start message
     jio_name = ' '.join(arguments)
     reply = get_open_jio_name_string(jio_name)
-    message = context.bot.send_message(chat_id, text=reply,
-                                       parse_mode='html')
-
+    message = context.bot.send_message(chat_id, text=reply, 
+                parse_mode= 'html')
+    
     # Initialise jio name and meta values
-    metadata = {'title': jio_name,
-                'message_id': message.message_id, 'finalised': 0}
+    metadata = {'title': jio_name, 'message_id': message.message_id, 'finalised': 0}
     r.hmset(chat_id_meta_string, metadata)
-
+    
     # Delete any existing jio in the group chat
     r.delete(chat_id_item_string)
-
+    
 # Edit the title of the jio
+def edit_jio_title(update, context): 
+    r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
 
-
-def edit_jio_title(update, context):
-    r = redis.from_url(os.environ.get("REDIS_URL"))
     arguments, user_id, user_name, chat_id = parse(update, context)
-    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(
-        chat_id, user_id)
+    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(chat_id, user_id)
 
     # Guard clause against user edited message
     if user_id == 0 or user_name == '':
         return
-
+    
     if not r.hget(chat_id_meta_string, 'message_id'):
         context.bot.send_message(chat_id=chat_id,
                                  text="No jio currently. Start a jio with '/start_jio <title>'")
         return
-
+    
     if not arguments:
         context.bot.send_message(chat_id=chat_id,
                                  text="To edit jio title, type '/edit_jio_title <new_jio_title>'")
@@ -93,38 +85,35 @@ def edit_jio_title(update, context):
         # Get updated jio title
         jio_name = ' '.join(arguments)
         message_id = int(r.hget(chat_id_meta_string, 'message_id'))
-
+        
         # Change jio name and meta values
         metadata = r.hgetall(chat_id_meta_string)
         metadata['title'] = jio_name
         r.hmset(chat_id_meta_string, metadata)
-
+        
         # Form updated message
         orders = r.hgetall(chat_id_item_string)
         orders_string = '\n'.join(['%s' % value for value in orders.values()])
         reply = get_open_jio_name_string(jio_name) + orders_string
 
-        context.bot.edit_message_text(
-            chat_id=chat_id, message_id=message_id, text=reply, parse_mode='html')
+        context.bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=reply, parse_mode= 'html')
 
-# Join a jio or update user's jio item
-
-
+# Join a jio or update user's jio item      
 def join_jio(update, context):
-    r = redis.from_url(os.environ.get("REDIS_URL"))
+    r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
+    
     arguments, user_id, user_name, chat_id = parse(update, context)
-    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(
-        chat_id, user_id)
-
+    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(chat_id, user_id)
+    
     # Guard clause against user edited message
     if user_id == 0 or user_name == '':
         return
-
+    
     if not r.hget(chat_id_meta_string, 'message_id'):
         context.bot.send_message(chat_id=chat_id,
                                  text="No jio currently. Start a jio with '/start_jio <title>'")
         return
-
+    
     message_id = int(r.hget(chat_id_meta_string, 'message_id'))
     finalised = int(r.hget(chat_id_meta_string, 'finalised'))
 
@@ -141,7 +130,7 @@ def join_jio(update, context):
         # Do nothing if updated item is the same as initial item
         if user_id_string in orders and order_message == orders[user_id_string]:
             return
-
+        
         # Update item
         orders[user_id_string] = order_message
         r.hmset(chat_id_item_string, orders)
@@ -151,22 +140,19 @@ def join_jio(update, context):
         jio_name = r.hget(chat_id_meta_string, 'title')
         reply = get_open_jio_name_string(jio_name) + updated_orders
 
-        context.bot.edit_message_text(
-            chat_id=chat_id, message_id=message_id, text=reply, parse_mode='html')
+        context.bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=reply, parse_mode= 'html')
 
 # Quit a joined jio before it is finalised
+def quit_jio(update, context):   
+    r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
 
-
-def quit_jio(update, context):
-    r = redis.from_url(os.environ.get("REDIS_URL"))
     arguments, user_id, user_name, chat_id = parse(update, context)
-    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(
-        chat_id, user_id)
+    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(chat_id, user_id)
 
     # Guard clause against user edited message
     if user_id == 0 or user_name == '':
         return
-
+    
     if not r.hget(chat_id_meta_string, 'message_id'):
         context.bot.send_message(chat_id=chat_id,
                                  text="No jio currently. Start a jio with '/start_jio <title>'")
@@ -178,39 +164,35 @@ def quit_jio(update, context):
 
     if user_id_string not in orders:
         context.bot.send_message(chat_id=chat_id,
-                                 text="Sorry, you are not in the current jio. 😢")
+                                 text="Sorry, you are not in the current jio. 😢") 
     else:
         original_message = orders[user_id_string]
 
         if finalised:
             context.bot.send_message(chat_id=chat_id,
-                                     text="You cannot quit a jio after it is finalised.")
+                                 text="You cannot quit a jio after it is finalised.")
         else:
             del orders[user_id_string]
             r.hdel(chat_id_item_string, user_id_string)
 
             # Form edited message
             jio_name = r.hget(chat_id_meta_string, 'title')
-            updated_orders = '\n'.join(
-                ['%s' % value for value in orders.values()])
+            updated_orders = '\n'.join(['%s' % value for value in orders.values()])
             reply = get_open_jio_name_string(jio_name) + updated_orders
 
-            context.bot.edit_message_text(
-                chat_id=chat_id, message_id=message_id, text=reply, parse_mode='html')
+            context.bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=reply, parse_mode= 'html')
 
 # Mark user's order as paid
-
-
 def paid(update, context):
-    r = redis.from_url(os.environ.get("REDIS_URL"))
+    r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
+
     arguments, user_id, user_name, chat_id = parse(update, context)
-    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(
-        chat_id, user_id)
+    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(chat_id, user_id)
 
     # Guard clause against user edited message
     if user_id == 0 or user_name == '':
         return
-
+    
     if not r.hget(chat_id_meta_string, 'message_id'):
         context.bot.send_message(chat_id=chat_id,
                                  text="No jio currently. Start a jio with '/start_jio <title>'")
@@ -243,34 +225,31 @@ def paid(update, context):
         jio_name = r.hget(chat_id_meta_string, 'title')
         updated_orders = '\n'.join(['%s' % value for value in orders.values()])
         reply = get_finalised_jio_name_string(jio_name) + updated_orders
-        context.bot.edit_message_text(
-            chat_id=chat_id, message_id=message_id, text=reply, parse_mode='html')
+        context.bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=reply, parse_mode= 'html')
 
         # Check if everyone paid, if so, end the jio.
         everyone_paid = True
         for order in orders.values():
             if '✅' not in order:
                 everyone_paid = False
-
+        
         if everyone_paid:
             r.delete(chat_id_meta_string)
             r.delete(chat_id_item_string)
             context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text="Everyone paid! Jio session ended. 🥳")
-
+                                text="Everyone paid! Jio session ended. 🥳")
+    
 # End a jio in the group
-
-
 def end_jio(update, context):
-    r = redis.from_url(os.environ.get("REDIS_URL"))
+    r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
+
     arguments, user_id, user_name, chat_id = parse(update, context)
-    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(
-        chat_id, user_id)
+    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(chat_id, user_id)
 
     # Guard clause against user edited message
     if user_id == 0 or user_name == '':
         return
-
+    
     if not r.hget(chat_id_meta_string, 'message_id'):
         context.bot.send_message(chat_id=chat_id,
                                  text="No jio currently. Start a jio with '/start_jio <title>'")
@@ -282,39 +261,42 @@ def end_jio(update, context):
     r.delete(chat_id_meta_string)
     r.delete(chat_id_item_string)
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Your current jio '" + jio_name + "' is terminated. See you next time!")
+                            text="Your current jio " + "is terminated. See you next time!")
 
 # Finalise the jio so that users can mark their orders as paid
-
-
 def finalise_jio(update, context):
-    r = redis.from_url(os.environ.get("REDIS_URL"))
+    r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
+
     arguments, user_id, user_name, chat_id = parse(update, context)
-    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(
-        chat_id, user_id)
+    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(chat_id, user_id)
 
     # Guard clause against user edited message
     if user_id == 0 or user_name == '':
         return
-
+    
     if not r.hget(chat_id_meta_string, 'message_id'):
         context.bot.send_message(chat_id=chat_id,
                                  text="No jio currently. Start a jio with '/start_jio <title>'")
         return
-
+    
     orders = r.hgetall(chat_id_item_string)
-    metadata = r.hgetall(chat_id_meta_string)
+    metadata =  r.hgetall(chat_id_meta_string)
 
+    if metadata['finalised'] == 1:
+        context.bot.send_message(chat_id=chat_id,
+                                 text="The jio is empty. To end the jio, use '/end_jio'")
+        return
+    
     if not orders:
         context.bot.send_message(chat_id=chat_id,
                                  text="The jio is empty. To end the jio, use '/end_jio'")
         return
-
+    
     # Form new message
     # jio_name = r.hget(chat_id_meta_string, 'title')
     # orders = '\n'.join(['%s' % value for value in orders.values()])
     reply = "Are you sure you want to finalise the jio?"
-
+    
     keyboard = [
         [
             InlineKeyboardButton("Yes", callback_data='confirm_finalise'),
@@ -323,48 +305,43 @@ def finalise_jio(update, context):
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    message = context.bot.send_message(
-        chat_id, text=reply, parse_mode='html', reply_markup=reply_markup)
+    message = context.bot.send_message(chat_id, text=reply, parse_mode= 'html', reply_markup=reply_markup)
 
     # Update message_id to the finalised message
     metadata['message_id'] = message.message_id
     r.hmset(chat_id_meta_string, metadata)
 
-
 def cancel(update, context):
-    r = redis.from_url(os.environ.get("REDIS_URL"))
+    r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
     arguments, user_id, user_name, chat_id = parse(update, context)
-    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(
-        chat_id, user_id)
+    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(chat_id, user_id)
     message_id = int(r.hget(chat_id_meta_string, 'message_id'))
 
-    context.bot.edit_message_text(
-        chat_id=chat_id, message_id=message_id, text="Don't play play ah! 😠")
+    context.bot.edit_message_text(chat_id=chat_id, message_id=message_id, text="Don't play play ah! 😠")
 
 
 # Confirmation to finalise jio, finalising a jio is irreversible
 def confirm_finalise_jio(update, context):
-    r = redis.from_url(os.environ.get("REDIS_URL"))
+    r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
+
     arguments, user_id, user_name, chat_id = parse(update, context)
-    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(
-        chat_id, user_id)
+    chat_id_meta_string, chat_id_item_string, user_id_string = stringify_ids(chat_id, user_id)
 
     # # Guard clause against user edited message
     # if user_id == 0 or user_name == '':
     #     return
-
+    
     orders = r.hgetall(chat_id_item_string)
-    metadata = r.hgetall(chat_id_meta_string)
-
+    metadata =  r.hgetall(chat_id_meta_string)
+    
     # Form new message
     jio_name = r.hget(chat_id_meta_string, 'title')
     orders = '\n'.join(['%s' % value for value in orders.values()])
     reply = get_finalised_jio_name_string(jio_name) + orders
     message_id = int(r.hget(chat_id_meta_string, 'message_id'))
 
-    message = context.bot.edit_message_text(
-        chat_id=chat_id, message_id=message_id, text=reply, parse_mode='html')
-
+    message = context.bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=reply, parse_mode= 'html')
+    
     # Set finalised to true
     metadata['finalised'] = 1
 
--- jiojiobot.py
+++ jiojiobot.py
@@ -1,34 +1,27 @@
-import redis
-from telegram.ext import Updater, CallbackQueryHandler, CallbackContext
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler, Filters
-import requests
-import logging
 from functions import *
 import os
-PORT = int(os.environ.get('PORT', '5000'))
-# ON_HEROKU = os.environ.get('ON_HEROKU')
-# if ON_HEROKU:
-#     # get the heroku port
-#     # as per OP comments default is 17995
-#     port = int(os.environ.get("PORT", 17995))
-# else:
-#     port = 6379  # 3000
-# PORT = int(os.environ.get('PORT', 8443))
+import redis
 
 # r = redis.Redis(host='localhost', port=6379)
-r = redis.from_url(os.environ.get("REDIS_URL"))
-bot_token = "1843778002:AAFxOoXX7xWTubiMmULqkTlQ9U0267zHa4I"
 
+import logging
+import requests
+
+from telegram.ext import MessageHandler, Filters
+from telegram.ext import CommandHandler
+from telegram.ext import Updater, CallbackQueryHandler, CallbackContext
+
+bot_token = "1843778002:AAFxOoXX7xWTubiMmULqkTlQ9U0267zHa4I"
+PORT = int(os.environ.get('PORT', 5000))
 
 # Setting up our logger
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+                    level=logging.INFO)
 
 
 def main():
-    r = redis.from_url(os.environ.get("REDIS_URL"))
+    r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
+    
     # Setting up their polling stuff
     updater = Updater(token=bot_token, use_context=True)
     dispatcher = updater.dispatcher
@@ -58,25 +51,21 @@ def main():
     quit_jio_handler = CommandHandler('quit_jio', quit_jio)
     dispatcher.add_handler(quit_jio_handler)
 
-    confirm_finalise_handler = CallbackQueryHandler(
-        callback=confirm_finalise_jio, pattern=r'confirm_finalise')
+    confirm_finalise_handler = CallbackQueryHandler(callback = confirm_finalise_jio, pattern = r'confirm_finalise' )
     dispatcher.add_handler(confirm_finalise_handler)
 
-    cancel_handler = CallbackQueryHandler(callback=cancel, pattern=r'cancel')
+    cancel_handler = CallbackQueryHandler(callback = cancel, pattern = r'cancel' )
     dispatcher.add_handler(cancel_handler)
 
-    # updater.start_polling()
 
-    # # Start the Bot
+    # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=bot_token,
-                          webhook_url='https://floating-thicket-85827.herokuapp.com/' + bot_token)
-    # updater.bot.setWebhook(
-    #     'https://floating-thicket-85827.herokuapp.com/' + bot_token)
+                        port=int(PORT),
+                        url_path=bot_token)
 
-    updater.idle()
+    updater.bot.setWebhook('https://floating-thicket-85827.herokuapp.com/' + bot_token)
 
+    updater.idle() 
 
 if __name__ == '__main__':
     main()
